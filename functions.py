104a02f5c9c732cd9591389006b73009e6c59c7e
nirmal <nirmalmali225@gmail.com>
2023-11-04 23:47:36 +0545

    Initial commit

---
+++ functions.py
@@ -0,0 +1,19 @@
+FILEPATH = "todos.txt"
+
+
+def get_todos(filepath="todos.txt"):
+    """ Read a text file abd return the list of to-do items. """
+    with open(filepath) as file_local:
+        todos_local = file_local.readlines()
+    return todos_local
+
+
+def write_todos(todos_arg, filepath=FILEPATH):
+    """ Write the to-do item list in the text file """
+    with open(filepath, 'w') as file:
+        file.writelines(todos_arg)
+
+
+if __name__ == "__main__":
+    print("Hello")
+    print(get_todos())
