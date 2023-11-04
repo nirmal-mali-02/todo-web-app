85e5d405f28f8ed45eb334eb8cb75186418f5337
nirmal <nirmalmali225@gmail.com>
2023-11-05 00:12:54 +0545

    Added pages

--- web.py
+++ web.py
@@ -3,6 +3,7 @@ import functions
 
 todos = functions.get_todos()
 
+st.set_page_config(layout='wide')
 
 def add_todo():
     new_todo = st.session_state['new_todo'] + "\n"
@@ -12,7 +13,13 @@ def add_todo():
 
 st.title("My Todo App")
 st.subheader("This is leadin")
-st.write("This app is to increase your productivity.")
+st.write("This app is to increase your <strong>productivity.</strong>", unsafe_allow_html=True)
+
+st.text_input(label="Enter a todo: ",
+              placeholder="Add a new todo...",
+              on_change=add_todo, key='new_todo')
+
+st.write("<strong>Todos List: </strong>", unsafe_allow_html=True)
 
 for index, todo in enumerate(todos):
     checkbox = st.checkbox(todo, key=todo)
@@ -22,6 +29,3 @@ for index, todo in enumerate(todos):
         del st.session_state[todo]
         st.experimental_rerun()
 
-st.text_input(label="Enter a todo: ",
-              placeholder="Add a new todo...",
-              on_change=add_todo, key='new_todo')
